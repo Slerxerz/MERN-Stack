b0243cb0b8c263b8143e197749c290986cefc977
Slerxerz <slerxerz@gmail.com>
2024-04-13 23:07:33 +0545

    email-pw-verf

--- Backend/models/userModel.js
+++ Backend/models/userModel.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose')
 const bcrypt = require('bcrypt')
+const validator = require('validator')
 
 const Schema = mongoose.Schema
 
@@ -18,6 +19,17 @@ const userSchema = Schema({
 //static signup method
 userSchema.statics.signup = async function(email,password){
     
+    if(!email || !password){
+        throw Error('All fields must be filled')
+    }
+    
+    //validation
+    if (!validator.isEmail(email)){
+        throw Error('Email is not valid')
+    }
+    if (!validator.isStrongPassword(password)){
+        throw Error('Password is not strong enough. Please include at least one Capital letter, one number, and one symbol.')
+    }
     const exists = await this.findOne({email})
 
     if (exists) {
